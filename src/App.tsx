e81538b3022d52d83e54bb36e414f8b515caf9d1
majidcoding <majidnadzir04@gmail.com>
2026-05-17 13:50:11 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GameStateProvider } from './src/context/GameStateContext';
 import { SplashLogin } from './src/screens/SplashLogin';
 import { Survey } from './src/screens/Survey';
